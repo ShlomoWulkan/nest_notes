1c460cbfb95f05078451138cc331d06cfb9205d7
shlomo <shlomo@dev.com>
2024-10-15 12:21:16 +0300

    register user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Model } from 'mongoose';
+import { User } from './entities/user .interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel('User') private UserModule: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<void> {
+    try {
+      const ahshPassword = await bcrypt.hash(createUserDto.password, 10);
+      const user = new this.UserModule({
+        username: createUserDto.username,
+        password: ahshPassword,
+        email: createUserDto.email
+      });
+
+      await user.save();
+
+    } catch (error) {
+      console.log(error);
+      throw new BadRequestException(error);
+    } 
   }
 
   findAll() {
